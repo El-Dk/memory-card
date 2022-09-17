e93b84fda16cad8245573dae826dd93267a1a693
Brian Mariscal Rodriguez <briannish@gmail.com>
2022-09-17 18:43:33 -0400

    Show static cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Card from './components/Card';
+import cardsContent from './helpers/cardsContent';
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState(50);
+  const [maxScore, setMaxScore] = useState(100);
   return (
     <div className="App">
+      <Header
+        score={score}
+        maxScore={maxScore}
+      />
+      <div className="cards-container">
+        {cardsContent.map(
+          cardContent => (<Card name={cardContent.name} src={cardContent.src} key={cardContent._id} />)
+        )}
+      </div>
     </div>
   );
 }
