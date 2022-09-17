3d2cad0f2089a5c85641c78b7acc867492f54380
Brian Mariscal Rodriguez <briannish@gmail.com>
2022-09-17 19:32:50 -0400

    Add basic score and re-order functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import cardsContent from './helpers/cardsContent';
 import './App.css';
 
+const sortCards = (cards = []) => {
+  const newOrder = [];
+  let count = cards.length;
+  while(count > 0) {
+    const random = Math.floor(Math.random() * cards.length);
+    if (!newOrder.includes(random)) {
+      newOrder.push(random);
+      count--;
+    }
+  }
+  return newOrder.map(index => cardsContent[index]);
+};
+
 function App() {
-  const [score, setScore] = useState(50);
-  const [maxScore, setMaxScore] = useState(100);
+  const [score, setScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [cardsInGame, setCardsInGame] = useState(cardsContent);
+  const [cardsPicked, setCardsPicked] = useState([]);
+
+  useEffect(() => {
+    score > maxScore && setMaxScore(score);
+  }, [score]);
+
+  const modifyScore = (event) => {
+    setCardsInGame(sortCards(cardsInGame))
+    const { dataset: { value } } = event.currentTarget;
+    if (!cardsPicked.includes(value)) {
+      cardsPicked.push(value);
+      setScore(score + 1);
+    } else {
+      resetGame();
+    }
+  };
+
+  const resetGame= () => {
+    setScore(0);
+    setCardsPicked([]);
+  };
+
   return (
     <div className="App">
       <Header
@@ -14,8 +50,14 @@ function App() {
         maxScore={maxScore}
       />
       <div className="cards-container">
-        {cardsContent.map(
-          cardContent => (<Card name={cardContent.name} src={cardContent.src} key={cardContent._id} />)
+        {cardsInGame.map(
+          cardInGame => (
+            <Card
+              key={cardInGame._id}
+              cardData={cardInGame}
+              modifyScore={modifyScore}
+            />
+          )
         )}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import './Card.css'
 
 const Card = (props) => {
-  return (<div className="card">
+  const { cardData } = props;
+  return (<div className="card" onClick={props.modifyScore} data-value={cardData._id}>
     <div className="card-img-container">
-      <img className="card-img" src={props.src} />
+      <img className="card-img" src={cardData.src} />
     </div>
-    <span className="card-name">{props.name}</span>
+    <span className="card-name">{cardData.name}</span>
   </div>);
 };
 
