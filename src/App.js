56d77ea514bd45e0e989e0de5fa4f62b7c18bea9
Brian Mariscal Rodriguez <briannish@gmail.com>
2022-09-18 03:08:24 -0400

    Add level up logic

--- src/App.js
+++ src/App.js
@@ -17,15 +17,43 @@ const sortCards = (cards = []) => {
   return newOrder.map(index => cardsContent[index]);
 };
 
+const levels = [
+  {
+    pointsToWin: 4
+  },
+  {
+    pointsToWin: 13
+  }
+  // {
+  //   pointsToWin: 29
+  // }
+];
+
 function App() {
   const [score, setScore] = useState(0);
   const [maxScore, setMaxScore] = useState(0);
-  const [cardsInGame, setCardsInGame] = useState(cardsContent);
+  const [cardsInGame, setCardsInGame] = useState(cardsContent.slice(0, levels[0].pointsToWin));
   const [cardsPicked, setCardsPicked] = useState([]);
+  const [level, setLevel] = useState(0);
+  const [win, setWin] = useState(false);
+  const maxLevel = levels.length;
 
   useEffect(() => {
-    score > maxScore && setMaxScore(score);
+    (score > maxScore) && setMaxScore(score);
   }, [score, maxScore]);
+
+  useEffect(() => {
+    (level < maxLevel) && (score >= levels[level].pointsToWin) && setLevel(lvl => lvl + 1);
+  }, [score, level, maxLevel]);
+
+  useEffect(() => {
+    if (level < levels.length) {
+      setCardsInGame(cardsContent.slice(0, levels[level].pointsToWin));
+      setCardsPicked([]);
+    } else {
+      setWin(true);
+    }
+  }, [level]);
 
   const modifyScore = (event) => {
     setCardsInGame(sortCards(cardsInGame))
@@ -41,27 +69,39 @@ function App() {
   const resetGame= () => {
     setScore(0);
     setCardsPicked([]);
+    setLevel(0);
+    setCardsInGame(cardsContent.slice(0, levels[0].pointsToWin));
+    setWin(false);
   };
 
-  return (
-    <div className="App">
-      <Header
-        score={score}
-        maxScore={maxScore}
-      />
-      <div className="cards-container">
-        {cardsInGame.map(
-          cardInGame => (
-            <Card
-              key={cardInGame._id}
-              cardData={cardInGame}
-              modifyScore={modifyScore}
-            />
-          )
-        )}
+  if (!win) {
+    return (
+      <div className="App">
+        <Header
+          score={score}
+          maxScore={maxScore}
+        />
+        <div className="cards-container">
+          {cardsInGame.map(
+            cardInGame => (
+              <Card
+                key={cardInGame._id}
+                cardData={cardInGame}
+                modifyScore={modifyScore}
+              />
+            )
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        You won!
+        <button onClick={() => resetGame()}>Reset</button>
+      </div>
+    );
+  }
 }
 
 export default App;
